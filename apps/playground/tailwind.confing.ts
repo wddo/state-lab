ca621c4c70cbfbba9528e4328b59096914fa927d
wddo <wddoddo@gmail.com>
2025-04-15 17:55:58 +0900

    add tailwindcss

---
+++ apps/playground/tailwind.confing.ts
@@ -0,0 +1,11 @@
+// apps/playground/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
