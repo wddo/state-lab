f6035defae30238f530319c346d99fdec1c44776
wddo <wddoddo@gmail.com>
2025-04-16 17:54:59 +0900

    vite.config

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,8 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      {
+        find: "@repo/ui",
+        replacement: path.resolve(__dirname, "../../packages/ui/src"),
+      },
+    ],
+  },
 });
