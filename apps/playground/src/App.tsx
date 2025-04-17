d1c8cd1b62c8d02d780afe11055f4df0c6448f45
wddo <wddoddo@gmail.com>
2025-04-17 15:25:50 +0900

    useCounterStore

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,9 +1,19 @@
+import { useCounterStore } from "@repo/store/zustand";
 import { Counter } from "@repo/ui/components/counter";
 
 function App() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const reset = useCounterStore((state) => state.reset);
+
   return (
     <div className="p-1 bg-test-pink">
-      <Counter counter={1} className="border-5 border-black bg-test-red" />
+      <Counter
+        counter={count}
+        onIncrement={increment}
+        onReset={reset}
+        className="border-5 border-black bg-test-red"
+      />
     </div>
   );
 }
