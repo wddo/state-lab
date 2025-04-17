25e62be0d8b6080ab8dc21eb7e4fb22447b56c45
wddo <wddoddo@gmail.com>
2025-04-17 16:27:00 +0900

    jotai

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,11 +1,22 @@
+import { counterAtom, incrementAtom, resetAtom } from "@repo/store";
 import { Counter } from "@repo/ui/components/counter";
 import { TodoList } from "@repo/ui/components/todo";
+import { useAtom } from "jotai";
 
 function App() {
+  const [count] = useAtom(counterAtom);
+  const [, increment] = useAtom(incrementAtom);
+  const [, reset] = useAtom(resetAtom);
+
   return (
     <div className="space-y-4">
       <div className="bg-test-pink p-1">
-        <Counter counter={1} className="border-5 bg-test-red border-black" />
+        <Counter
+          counter={count}
+          onIncrement={increment}
+          onReset={reset}
+          className="border-5 bg-test-red border-black"
+        />
       </div>
 
       <div>
