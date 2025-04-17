f440b030511d37d5e922b56a13ef36ac4300c596
wddo <wddoddo@gmail.com>
2025-04-17 16:27:00 +0900

    jotai

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,9 +1,20 @@
+import { counterAtom, incrementAtom, resetAtom } from "@repo/store";
 import { Counter } from "@repo/ui/components/counter";
+import { useAtom } from "jotai";
 
 function App() {
+  const [count] = useAtom(counterAtom);
+  const [, increment] = useAtom(incrementAtom);
+  const [, reset] = useAtom(resetAtom);
+
   return (
     <div className="p-1 bg-test-pink">
-      <Counter counter={1} className="border-5 border-black bg-test-red" />
+      <Counter
+        counter={count}
+        onIncrement={increment}
+        onReset={reset}
+        className="border-5 border-black bg-test-red"
+      />
     </div>
   );
 }
