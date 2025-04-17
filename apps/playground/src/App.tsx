0c6825d453e3780c9cae823eb3c9480364741ccc
wddo <wddoddo@gmail.com>
2025-04-17 15:25:50 +0900

    useCounterStore

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,11 +1,21 @@
+import { useCounterStore } from "@repo/store/zustand";
 import { Counter } from "@repo/ui/components/counter";
 import { TodoList } from "@repo/ui/components/todo";
 
 function App() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const reset = useCounterStore((state) => state.reset);
+
   return (
     <div className="space-y-4">
       <div className="bg-test-pink p-1">
-        <Counter counter={1} className="border-5 bg-test-red border-black" />
+        <Counter
+          counter={count}
+          onIncrement={increment}
+          onReset={reset}
+          className="border-5 bg-test-red border-black"
+        />
       </div>
 
       <div>
