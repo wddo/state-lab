d2104caf8d9c9de816e514c4f44fed2f4cba1a03
wddo <wddoddo@gmail.com>
2025-04-21 11:39:57 +0900

    components/todo

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,9 +1,16 @@
 import { Counter } from "@repo/ui/components/counter";
+import { TodoList } from "@repo/ui/components/todo";
 
 function App() {
   return (
-    <div className="p-1 bg-test-pink">
-      <Counter counter={1} className="border-5 border-black bg-test-red" />
+    <div className="space-y-4">
+      <div className="bg-test-pink p-1">
+        <Counter counter={1} className="border-5 bg-test-red border-black" />
+      </div>
+
+      <div>
+        <TodoList />
+      </div>
     </div>
   );
 }
