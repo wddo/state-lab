62fcb71608d08b31eb3ec77784c1218a68616710
wddo <wddoddo@gmail.com>
2025-06-11 11:17:38 +0900

    vitest 환경 구성

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,14 @@
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@repo/ui": path.resolve(__dirname, "./src"), // 실제 경로에 맞게 수정
+    },
+  },
+  test: {
+    include: ["src/**/*.test.{ts,tsx}"],
+    environment: "jsdom",
+  },
+});
