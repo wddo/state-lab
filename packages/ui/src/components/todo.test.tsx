d537f4e2488c7862e239be0c377006b0f963bd4f
wddo <wddoddo@gmail.com>
2025-06-12 17:28:11 +0900

    vitest TodoList mock 개선 ( api 직접 spy )

--- packages/ui/src/components/todo.test.tsx
+++ packages/ui/src/components/todo.test.tsx
@@ -1,65 +1,60 @@
 import { afterEach, expect, test, vi } from "vitest";
 
-import { withQueryClient } from "@repo/ui/test-utils";
 import { cleanup, render, screen, waitFor } from "@testing-library/react";
 
+import * as todoApi from "@repo/api/todo";
+import { withQueryClient } from "@repo/ui/test-utils";
 import userEvent from "@testing-library/user-event";
 import { TodoList } from "./todo/TodoList";
 
-const fetchSpy = vi.spyOn(global, "fetch");
+const mockFetch = vi.spyOn(todoApi, "fetchTodoApi");
+const mockCreate = vi.spyOn(todoApi, "createTodoApi");
+const mockUpdate = vi.spyOn(todoApi, "updateTodoByIdApi");
+const mockDelete = vi.spyOn(todoApi, "deleteTodoByIdApi");
 
 afterEach(() => {
   cleanup();
 });
 
 test("TodoList가 정상적으로 렌더링되어야 합니다", async () => {
-  fetchSpy.mockResolvedValueOnce({
-    json: () =>
-      Promise.resolve([
-        { id: "1", title: "기존 할 일 1" },
-        { id: "2", title: "기존 할 일 2" },
-      ]),
-  } as Response);
+  mockFetch.mockResolvedValueOnce([
+    { id: "1", title: "할 일 1" },
+    { id: "2", title: "할 일 2" },
+  ]);
 
   render(withQueryClient(<TodoList />));
 
   await waitFor(() => {
-    expect(screen.getByDisplayValue("기존 할 일 1")).toBeTruthy();
-    expect(screen.getByDisplayValue("기존 할 일 2")).toBeTruthy();
+    expect(screen.getByDisplayValue("할 일 1")).toBeTruthy();
+    expect(screen.getByDisplayValue("할 일 2")).toBeTruthy();
   });
 });
 
 test("TodoList에 새로운 할 일을 추가할 수 있어야 합니다", async () => {
-  fetchSpy.mockResolvedValueOnce({
-    json: () =>
-      Promise.resolve([
-        { id: "1", title: "기존 할 일 1" },
-        { id: "2", title: "기존 할 일 2" },
-      ]),
-  } as Response);
+  mockFetch.mockResolvedValueOnce([
+    { id: "1", title: "기존 할 일 1" },
+    { id: "2", title: "기존 할 일 2" },
+  ]);
 
   render(withQueryClient(<TodoList />));
   const user = userEvent.setup();
 
-  const input = screen.getByPlaceholderText("할 일을 입력하세요");
+  const input = await screen.findByPlaceholderText("할 일을 입력하세요");
   await user.type(input, "새로운 할 일");
 
+  // react-query 의 invalidateQueries 에 의해 refetch 발생시키기 위함
+  mockCreate.mockResolvedValueOnce(undefined);
+
+  mockFetch.mockResolvedValueOnce([
+    { id: "1", title: "기존 할 일 1" },
+    { id: "2", title: "기존 할 일 2" },
+    { id: "3", title: "새로운 할 일" },
+  ]);
+
   const addButton = await screen.findByRole("button", { name: "추가" });
-
-  fetchSpy.mockResolvedValueOnce({
-    json: () =>
-      Promise.resolve([
-        { id: "1", title: "기존 할 일 1" },
-        { id: "2", title: "기존 할 일 2" },
-        { id: "3", title: "새로운 할 일" },
-      ]),
-  } as Response);
-
   await user.click(addButton);
 
   await waitFor(() => {
     expect(screen.getByDisplayValue("새로운 할 일")).toBeTruthy();
   });
-
-  console.log(screen.logTestingPlaygroundURL());
 });
--- packages/ui/src/test-utils.tsx
+++ packages/ui/src/test-utils.tsx
@@ -1,15 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      gcTime: 0,
-    },
-  },
-});
-
 export function withQueryClient(children: React.ReactNode) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false, // 재시도 방지
+        gcTime: 0, // 캐시 즉시 정리
+        staleTime: 0, // 즉시 stale로 만들어서 refetch 유도
+      },
+      mutations: {
+        retry: false, // 재시도 방지
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
