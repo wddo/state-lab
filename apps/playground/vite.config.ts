ab9ba1101b00ceb3bf7f30a7812aba298cdf8f01
wddo <wddoddo@gmail.com>
2025-06-20 16:34:25 +0900

    deploy.yml 생성

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/state-lab/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
