0fe15057b3f5cc8d7dbe709876908fc6c4249bf3
wddo <wddoddo@gmail.com>
2025-07-22 17:22:56 +0900

    fix: TODO_URI 수정

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -1,6 +1,6 @@
 export const TODO_URI =
   process.env.NODE_ENV === "production"
-    ? "https://state-lab-playground.vercel.app/api/todo"
+    ? "https://state-lab-api.vercel.app/api/todo"
     : "http://localhost:5001/todos";
 
 export const TODO_QUERY_KEY = "todo";
