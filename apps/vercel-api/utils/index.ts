412608e8d9583d6e3849c5bd537a094d7940ee52
wddo <wddoddo@gmail.com>
2025-08-29 13:10:44 +0900

    Redis 토큰 변경

--- apps/vercel-api/utils/index.ts
+++ apps/vercel-api/utils/index.ts
@@ -2,8 +2,8 @@ import { Redis } from "@upstash/redis";
 
 // Redis 인스턴스
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
 });
 
 // 공통 헤더 설정
