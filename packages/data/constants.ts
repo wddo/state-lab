c46a68c5590e0ec1af50e8c39d34b02f5c9454fa
wddo <wddoddo@gmail.com>
2025-09-05 13:39:01 +0900

    fix: api uri 변경

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -1,6 +1,6 @@
 export const TODO_URI =
   process.env.NODE_ENV === "production"
-    ? "https://state-lab-api.vercel.app/todos"
+    ? "https://state-lab-server.vercel.app/todos"
     : "http://localhost:5001/todos";
 
 export const TODO_QUERY_KEY = "todo";
